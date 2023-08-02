5e14ee321674ae53beb8c927e7d445e814fc4f54
ChethanUK <chethanuk@outlook.com>
2023-08-02 15:42:54 +0000

    Adding DBReader and writer

--- seedspark/spark/__init__.py
+++ seedspark/spark/__init__.py
@@ -1,13 +1,33 @@
 # -*- coding: utf-8 -*-
 
+# from seedspark.spark.db.onetl import DBReader, DBWriter, Postgres, SnapshotStrategy,
+# SnapshotBatchStrategy, IncrementalStrategy, IncrementalBatchStrategy
+from seedspark.spark.db.onetl import (
+    DBReader,
+    DBWriter,
+    IncrementalBatchStrategy,
+    IncrementalStrategy,
+    Postgres,
+    SnapshotBatchStrategy,
+    SnapshotStrategy,
+)
+
 onetl = [
     "DBReader",
     "DBWriter",
     "Postgres",
-    "SnapshotBatchStrategy",
+    "SnapshotStrategy",
     "SnapshotBatchStrategy",
     "IncrementalStrategy",
     "IncrementalBatchStrategy",
 ]
 
-__all__ = ["SparkAppsException", "SparkApps", "DataFrameUtils"] + onetl
+__all__ = ["SparkAppsException", "SparkApps", "DataFrameUtils"] + [
+    "DBReader",
+    "DBWriter",
+    "Postgres",
+    "SnapshotStrategy",
+    "SnapshotBatchStrategy",
+    "IncrementalStrategy",
+    "IncrementalBatchStrategy",
+]
--- tests/test_imports/test_on_etl.py
+++ tests/test_imports/test_on_etl.py
@@ -1,12 +1,35 @@
 import pytest
 
-
-# Test function for importing SeedSpark onETL Spark modules
-@pytest.mark.parametrize(
-    "module_name", ["DBReader", "DBWriter", "Postgres", "IncrementalStrategy", "SnapshotBatchStrategy"]
+from seedspark.spark import (
+    DBReader,
+    DBWriter,
+    IncrementalBatchStrategy,
+    IncrementalStrategy,
+    Postgres,
+    SnapshotBatchStrategy,
+    SnapshotStrategy,
 )
-def test_imports(module_name):
-    try:
-        __import__(f"seedspark.spark.{module_name}")
-    except ImportError:
-        pytest.fail(f"Failed to import {module_name}")
+
+
+@pytest.mark.parametrize(
+    "module",
+    [
+        DBReader,
+        Postgres,
+        DBWriter,
+        IncrementalBatchStrategy,
+        IncrementalStrategy,
+        SnapshotBatchStrategy,
+        SnapshotStrategy,
+    ],
+)
+def test_import(module):
+    expected_name = f"seedspark.spark.db.onetl.{module.__name__}"
+    imported_name = f"{module.__module__}.{module.__name__}"
+
+    print(f"Expected name: {expected_name}")
+    print(f"Imported name: {imported_name}")
+
+    assert (
+        imported_name == expected_name
+    ), f"Imported module name '{imported_name}' doesn't match the expected name '{expected_name}'"
